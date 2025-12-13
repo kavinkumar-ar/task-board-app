e53f12da444c8e62bcffe62d686edbe2253b98d3
KAVINKUMAR A R <150226309+kavinkumar-ar@users.noreply.github.com>
2025-12-13 13:34:19 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'process.env.https://task-board-app-bice.vercel.app+"/api/data"';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
